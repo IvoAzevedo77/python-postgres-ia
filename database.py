76dd40aee4e5ba49cfd05e09baae4e0d176946aa
Ivo“git config --global user.email “ivofco@gmail.com <ivo.azevedo.lec_a@msft.cesae.pt>
2024-05-17 15:36:27 +0100

    add database4

--- database.py
+++ database.py
@@ -1,20 +1,35 @@
 import psycopg2
 import os
-import sys
-#print(os.environ)
 
+# Obtém as informações de conexão a partir das variáveis de ambiente
+dbname = "db-ia"
+user = os.getenv("USER")
+password = os.getenv("PRIVATEKEYPOSTGRES")
+host = "database-ea.cvkei0o2ei4o.eu-central-1.rds.amazonaws.com"
+port = "5432"
+
+# Conecta-se ao banco de dados
 conn = psycopg2.connect(
-    dbname="db-ia",
-    user="postgres",
-    password="Abcd1234!",
-    host="database-ea.cvkei0o2ei4o.eu-central-1.rds.amazonaws.com",
-    port="5432"
+    dbname=dbname,
+    user=user,
+    password=password,
+    host=host,
+    port=port
 )
 
+# Cria um cursor para executar comandos SQL
 cursor = conn.cursor()
+
+# Executa a consulta
 cursor.execute('SELECT * FROM public.contacts;')
+
+# Recupera os resultados
 results = cursor.fetchall()
+
+# Fecha o cursor e a conexão
+cursor.close()
 conn.close()
 
+# Itera sobre os resultados e imprime-os
 for x in results:
     print(x)
